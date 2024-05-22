e0229a1c700286e5912f0bb65c4fdf94ec0a8a00
PeterStoffberg <64158097+PeterStoffberg@users.noreply.github.com>
2024-05-22 11:59:45 +0200

    now for loaders...

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,35 @@ let path = require('path');
 let webpack = require('webpack');
 
 module.exports = {
-    devtool: 'source-map',
-    entry: {
-        widget: [
-            path.join(__dirname, 'src', 'widget', 'widget-index.js')
-        ],
-        chat: [
-            path.join(__dirname, 'src', 'chat', 'chat-index.js')
-        ],
-    },
-    output: {
-        path: path.join(__dirname, 'dist', 'js'),
-        filename: '[name].js',
-        publicPath: '/js/'
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, loaders: ['babel'], include: path.join(__dirname, 'src') },
-            { test: /\.css$/, loader: 'style!css!sass', include: path.join(__dirname, 'css') },
-        ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        })
-    ]
+  mode: 'production',
+  devtool: 'source-map',
+  entry: {
+    widget: [path.join(__dirname, 'src', 'widget', 'widget-index.js')],
+    chat: [path.join(__dirname, 'src', 'chat', 'chat-index.js')],
+  },
+  output: {
+    path: path.join(__dirname, 'dist', 'js'),
+    filename: '[name].js',
+    publicPath: '/js/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+        include: path.join(__dirname, 'src'),
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.join(__dirname, 'css'),
+      },
+    ],
+  },
 };
